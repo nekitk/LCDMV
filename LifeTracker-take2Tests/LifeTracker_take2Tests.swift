be437b5d026d8827850f10f23cf9b2a6c044525b
nekitk <golubov@neki.tk>
2014-06-28 12:52:53 +0400

    Initial Commit

---
+++ LifeTracker-take2Tests/LifeTracker_take2Tests.swift
@@ -0,0 +1,35 @@
+//
+//  LifeTracker_take2Tests.swift
+//  LifeTracker-take2Tests
+//
+//  Created by neekitkah on 28.06.14.
+//  Copyright (c) 2014 nekitk. All rights reserved.
+//
+
+import XCTest
+
+class LifeTracker_take2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
