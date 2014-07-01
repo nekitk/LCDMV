10e945ada33a1862507a1fe5e79b04eeb45e6ce1
nekitk <golubov@neki.tk>
2014-07-01 14:07:48 +0400

    Changing timer behavior to be able to return from inactive mode

--- LifeTracker-take2/CurrentTimerViewController.swift
+++ LifeTracker-take2/CurrentTimerViewController.swift
@@ -7,62 +7,49 @@ class CurrentTimerViewController: UIViewController {
     @IBOutlet var txtTime: UILabel
     
     var soundPlayer: AVAudioPlayer!
-    let finishSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("mario", ofType: "wav"))
     
-    var interval: Int = 0
-    var ticked = 0
-    var scheduledTimer: NSTimer? = nil
-    var isRunning = false
+    let finishSoundName = "mario.wav"
+    let finishSoundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("mario", ofType: "wav"))
+    
+    var timerTimeInSec: Int = 0
+    var launchMoment: NSDate?
+    
+    //todo nstimer: schedule at view appear and call updateTime()
     
     @IBAction func runButtonClick() {
-        if !isRunning {
-            scheduledTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTime", userInfo: nil, repeats: true)
-            isRunning = true
-            updateTimeLabel()
-            
-            soundPlayer.playAtTime(soundPlayer.deviceCurrentTime + NSTimeInterval(interval))
+        if !launchMoment {
+            launchMoment = NSDate()
+            updateTime()
+            scheduleNotification()
         }
+    }
+    
+    func scheduleNotification() {
+        let timerEndNotification = UILocalNotification()
+        timerEndNotification.fireDate = NSDate(timeIntervalSinceNow: NSTimeInterval(timerTimeInSec))
+        timerEndNotification.timeZone = NSTimeZone.defaultTimeZone()
+        timerEndNotification.alertBody = "Timer ended"
+        timerEndNotification.soundName = finishSoundName
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(timerEndNotification)
     }
     
     func updateTime() {
-        if ++ticked >= interval {
-            timerFinished()
-        }
-        else {
-            updateTimeLabel()
-        }
-    }
-    
-    func timerFinished() {
-        scheduledTimer?.invalidate()
-        txtTime.text = "Finished"
-        
-        timersManager.trackCurrent()
-        isRunning = false
-        ticked = 0
-    }
-    
-    func updateTimeLabel() {
-        txtTime.text = String(interval - ticked)
+        let secondsLeft = NSDate().timeIntervalSinceDate(launchMoment!)
+        txtTime.text = String(secondsLeft)
     }
     
     @IBAction func pauseButtonClick() {
-        if isRunning {
-            scheduledTimer?.invalidate()
-            isRunning = false
-        }
+
     }
     
     @IBAction func resetButtonClick() {
-        scheduledTimer?.invalidate()
-        ticked = 0
-        isRunning = false
-        updateTimeLabel()
+
     }
     
     override func viewDidLoad() {
         // Initializing audio player
-        soundPlayer = AVAudioPlayer(contentsOfURL: finishSound, error: nil)
+        soundPlayer = AVAudioPlayer(contentsOfURL: finishSoundURL, error: nil)
         soundPlayer.prepareToPlay()
     }
     
@@ -70,9 +57,12 @@ class CurrentTimerViewController: UIViewController {
         let timer = timersManager.getCurrent()
         if timer {
             txtName.text = timer.name
-            interval = 60 * timer.minutes + timer.seconds
-            updateTimeLabel()
+            timerTimeInSec = 60 * timer.minutes + timer.seconds
         }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        
     }
 
 }
